11d2d276e098bb8455d95ca0bff290946bee88c8
Pavel Biryukov <biryukovpyu@gmail.com>
2018-07-10 22:21:40 +0300

    add eslint

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-export default (question) => {
-	const name = readlineSync.question('May I have your name? ');
-	console.log(`Hello, ${name}!`);
+export default () => {
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
 };
