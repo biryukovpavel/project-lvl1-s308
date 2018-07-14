cf27dde2021154cfdfcf49c7f1d37a445448ce29
Pavel Biryukov <biryukovpyu@gmail.com>
2018-07-14 15:13:22 +0300

    add arithmetic progression game

---
+++ src/games/progression.js
@@ -0,0 +1,39 @@
+import getRandomNumber from '../common/randomNumber';
+import startGame, { makeQuestionAndAnswer } from '../gameEngine';
+
+const description = 'What number is missing in this progression?';
+
+const termsCount = 10;
+
+const progressionGenerator = (firstNumber, difference) => (
+  index => firstNumber + (index * difference)
+);
+const getProgression = (getProgressionElement, missingNumberIndex) => {
+  const iter = (elementsIndex, acc) => {
+    if (elementsIndex === termsCount) {
+      return acc;
+    }
+
+    const element = elementsIndex === missingNumberIndex ? '..' : getProgressionElement(elementsIndex);
+    return iter(elementsIndex + 1, `${acc} ${element}`);
+  };
+
+  return iter(0, '');
+};
+
+const getQuestionAndAnswer = () => {
+  const firstNumber = getRandomNumber();
+  const difference = getRandomNumber(1, 25);
+  const missingNumberIndex = getRandomNumber(0, termsCount - 1);
+
+  const getProgressionElement = progressionGenerator(firstNumber, difference);
+
+  const question = getProgression(getProgressionElement, missingNumberIndex);
+  const correctAnswer = getProgressionElement(missingNumberIndex);
+
+  return makeQuestionAndAnswer(question, correctAnswer);
+};
+
+export default () => {
+  startGame(description, getQuestionAndAnswer);
+};
